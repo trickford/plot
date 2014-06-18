e94fbb04b91911d9e88b54692af6a233ec37f3f6
Tom Grochowicz <tgrochowicz@gmail.com>
2014-06-18 16:00:00 -0500

    allowing initial range-setting

--- js/jquery.plot.js
+++ js/jquery.plot.js
@@ -8,7 +8,9 @@
 				"height": 300, // height of canvas
 				"type": "bar", // "bar" or "line"
 				"range": {
-					"show": true // show range selection
+					"show": true, // show range selection
+					"initialSelection": null 	// object representing the starting state of the selection range
+																	 	// { start: 0, end: 10 }
 				},
 				"grid": {
 					"show": true, // show grid
@@ -133,8 +135,9 @@
 		}
 
 		// load range handle images if that's what you want
+		var imagePromise;
 		if(self.config.style.handleImage){
-			self.loadRangeImage();
+			imagePromise = self.loadRangeImage();
 		}
 
 		// all that mousemove, mouseover, mouseout shit that makes this plugin better than yours
@@ -156,6 +159,13 @@
 		// setup labels if that's what you want
 		if(self.config.labels.show){
 			self.createLabels();
+		}
+
+
+		if(self.config.range.initialSelection) {
+			imagePromise.then(function() {
+				self.drawRange(self.$rangeCanvas, self.config.range.initialSelection.start, self.config.range.initialSelection.end);
+			});
 		}
 
 		// return to jQuery chain
@@ -1090,25 +1100,44 @@
 		var self = this;
 
 		if(typeof self.rangeCallback === "function"){
-			self.rangeCallback({from: self.range.from.data, to: self.range.to.data});
+			self.rangeCallback({from: self.range.from.data, to: self.range.to.data, raw: self.range});
 		}
 		// self.$el.data("range", self.range);
 	}
 
 	Plot.prototype.loadRangeImage = function(){
+
+		var promise = $.Deferred();
 		var self = this;
 		if (typeof self.config.style.handleImage === 'string') {
 			self.range.handles.image = new Image();
 			self.range.handles.image.src = self.config.style.handleImage;
+			promise.resolve();
 		}
 		else if (typeof self.config.style.handleImage === 'object') {
+			var leftPromise = $.Deferred(),
+					rightPromise = $.Deferred();
+
 			self.range.handles.leftImage = new Image();
+			self.range.handles.leftImage.onload = function(){
+				leftPromise.resolve();
+			}
 			self.range.handles.leftImage.src = self.config.style.handleImage.left;
 
+
 			self.range.handles.rightImage = new Image();
+			self.range.handles.rightImage.onload = function() {
+				rightPromise.resolve();
+			}
 			self.range.handles.rightImage.src = self.config.style.handleImage.right;
+
+			$.when(leftPromise, rightPromise).then(function() {
+				promise.resolve();
+			})
+
 		}
 
+		return promise;
 
 	}
 
